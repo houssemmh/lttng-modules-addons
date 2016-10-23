88a54edcb93bb7e99dcf9a6b95bf04d116a6cbab
Houssem Daoud <houssemmh@gmail.com>
2016-10-23 18:49:24 -0400

    periodic workqueue & hashmap creation

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 make -j8
-sudo make modules_install
-sudo depmod -a
-./control-addons.sh reload
+sudo insmod addons/lttng-meminfo.ko
+#sudo make modules_install
+#sudo depmod -a
+#./control-addons.sh reload
--- rsync.sh
+++ rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -v -r . virtualMachine:lttng-modules-addons/
+rsync -v -r addons virtualMachine:lttng-modules-addons/
 
