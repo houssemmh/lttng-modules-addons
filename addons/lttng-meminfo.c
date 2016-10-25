186a9dcae2e33747067314e6e8017eb8ceb0eabd
Houssem Daoud <houssemmh@gmail.com>
2016-10-25 12:39:27 -0400

    new tracepoint definition with vm

--- addons/lttng-meminfo.c
+++ addons/lttng-meminfo.c
@@ -40,36 +40,51 @@
 #include "../instrumentation/events/lttng-module/addons.h"
 
 DEFINE_TRACE(addons_process_meminfo);
-#define COUNTER_MAX 1000
+#define EVENTS_THRESHOLD 100
+#define DELAY 500
 
 extern struct task_struct init_task;
-int delay = 2000;
 struct workqueue_struct *meminfo_queue;
 static DEFINE_HASHTABLE(process_map, 3);
 
+/**
+ * process_key_t is the key of the hashtable.
+ * process_val_t contains information about the process
+ *
+ */
 struct process_key_t {
   pid_t tgid;
 } __attribute__((__packed__));
 
 struct process_val_t {
   pid_t tgid;
-  int counter;
+  int memEvents;
   struct hlist_node hlist;
   struct rcu_head rcu;
 };
 
-static struct Workqueue_data {
+/**
+ * wq_data is the workqueue used to generate periodic trace events.
+ */
+struct Workqueue_data {
 	struct delayed_work delayed_getMemInfo;
 	int delay;
-} wq_data;
+};
+struct Workqueue_data wq_data = {.delay = DELAY};
 
+/**
+ * Free process_val_t based on the rcu field
+ */
 static void free_process_val_rcu(struct rcu_head *rcu)
 {
   kfree(container_of(rcu, struct process_val_t, rcu));
 }
 
+/**
+ * Find a process inside the hashtable
+ */
 static struct process_val_t*
-find_process(struct process_key_t *key, u32 hash)
+find_process_ht(struct process_key_t *key, u32 hash)
 {
   struct process_val_t *val;
 
@@ -81,7 +96,10 @@ find_process(struct process_key_t *key, u32 hash)
   return NULL;
 }
 
-static void register_process(struct task_struct *p) {
+/**
+ * Insert a process in the hashtable
+ */
+static void insert_process_ht(struct task_struct *p) {
 	u32 hash;
 	struct process_key_t key;
 	struct process_val_t *val;
@@ -89,10 +107,13 @@ static void register_process(struct task_struct *p) {
 	hash = jhash(&key, sizeof(key), 0);
 	val = kzalloc(sizeof(struct process_val_t), GFP_KERNEL);
 	val->tgid = key.tgid;
-	val->counter = COUNTER_MAX;
+	val->memEvents = 0;
 	hash_add_rcu(process_map, &val->hlist, hash);
 }
 
+/**
+ * Show the content of the hashtable
+ */
 static void show_process_map(void) {
 	struct process_val_t *process_val;
 	int bkt;
@@ -104,6 +125,9 @@ static void show_process_map(void) {
 	rcu_read_unlock();
 }
 
+/**
+ * Clear the content of the hashtable
+ */
 static void clear_process_map(void) {
 	struct process_val_t *process_val;
 	int bkt;
@@ -117,9 +141,28 @@ static void clear_process_map(void) {
 	synchronize_rcu();
 }
 
+static void getProcessMemInfo(struct task_struct *p)
+{
+	long rss_anon, rss_files, total_rss, total_vm, swap;
+	struct mm_struct *mm;
+
+	mm = get_task_mm(p);
+	rss_anon = atomic_long_read(&mm->rss_stat.count[MM_ANONPAGES]) << (PAGE_SHIFT - 10);
+	rss_files = atomic_long_read(&mm->rss_stat.count[MM_FILEPAGES]) << (PAGE_SHIFT - 10);
+	total_rss = rss_anon + rss_files;
+	swap = atomic_long_read(&mm->rss_stat.count[MM_SWAPENTS]) << (PAGE_SHIFT - 10);
+	total_vm = mm->total_vm << (PAGE_SHIFT - 10);
+
+	//printk(KERN_INFO "process %s, pid:%d, rss: %lu\n", p->comm, p->pid, rss_kb);
+	trace_addons_process_meminfo(p->pid, total_rss, total_vm, rss_anon, rss_files);
+}
+
+/**
+ * Called periodically using a workqueue task
+ * Generate a trace event about memory usage of each process
+ */
 static void getMemInfo(struct work_struct *work) {
 	struct task_struct *p;
-	long rss_anon, rss_files, rss_kb;
 	struct mm_struct *mm;
 	clear_process_map();
 	rcu_read_lock();
@@ -127,41 +170,58 @@ static void getMemInfo(struct work_struct *work) {
 	{
 		mm = get_task_mm(p);
 		if (mm) {
-			register_process(p);
-			rss_anon = atomic_long_read(&mm->rss_stat.count[MM_ANONPAGES]);
-			rss_files = atomic_long_read(&mm->rss_stat.count[MM_FILEPAGES]);
-			rss_kb = (rss_anon + rss_files) << (PAGE_SHIFT - 10);
-			printk(KERN_INFO "process %s, pid:%d, rss: %lu\n", p->comm, p->pid,
-					rss_kb);
-			trace_addons_process_meminfo(p->pid, rss_kb);
+			insert_process_ht(p);
+			getProcessMemInfo(p);
 		}
 
 	}
 	rcu_read_unlock();
-	show_process_map();
 	queue_delayed_work(meminfo_queue, &wq_data.delayed_getMemInfo, msecs_to_jiffies(wq_data.delay));
 }
 
-//static int lttng_meminfo_probe(struct page *page, struct vm_area_struct *vma,
-//		unsigned long address) {
-//	printk(KERN_INFO "PROBE!\n");
-//out:
-//	jprobe_return();
-//	return 0;
-//
-//}
+static int lttng_meminfo_probe(struct page *page, struct vm_area_struct *vma,
+		unsigned long address) {
+	u32 hash;
+	struct process_key_t key;
+	struct process_val_t *val;
+	struct task_struct *p;
 
-//static struct jprobe meminfo_jprobe = { .entry = lttng_meminfo_probe, .kp = {
-//		.symbol_name = "page_move_anon_rmap", }, };
+	p = get_current();
+	key.tgid = p->tgid;
+	hash = jhash(&key, sizeof(key), 0);
+	val = find_process_ht(&key, hash);
+	if (val == NULL)
+		goto out;
 
-static int __init lttng_addons_meminfo_init(void) {
+	if(++val->memEvents >= EVENTS_THRESHOLD)
+	{
+		getProcessMemInfo(p);
+		val->memEvents = 0;
+	}
 
+
+out:
+	jprobe_return();
+	return 0;
+
+}
+
+static struct jprobe memalloc_jprobe = { .entry = lttng_meminfo_probe, .kp = {
+		.symbol_name = "__alloc_pages_nodemask", }, };
+
+static struct jprobe memfree_jprobe = { .entry = lttng_meminfo_probe, .kp = {
+		.symbol_name = "free_pages_prepare", }, };
+
+static int __init lttng_addons_meminfo_init(void)
+{
+	int ret;
+
+	ret = register_jprobe(&memalloc_jprobe);
+	ret = register_jprobe(&memfree_jprobe);
 	(void) wrapper_lttng_fixup_sig(THIS_MODULE);
-//	int ret;
-//	ret = register_jprobe(&meminfo_jprobe);
-	printk(KERN_INFO "Hello world!\n");
+	printk(KERN_INFO "Lttng_meminfo loaded\n");
 	meminfo_queue = create_workqueue("meminfo_queue");
-	wq_data.delay = delay;
+	wq_data.delay = DELAY;
 	INIT_DELAYED_WORK(&wq_data.delayed_getMemInfo, getMemInfo );
 	queue_delayed_work(meminfo_queue, &wq_data.delayed_getMemInfo, msecs_to_jiffies(wq_data.delay));
 
@@ -171,11 +231,14 @@ static int __init lttng_addons_meminfo_init(void) {
 module_init(lttng_addons_meminfo_init);
 
 static void __exit lttng_addons_meminfo_exit(void) {
+	/* cancel the delayed work and destroy the workqueue */
 	cancel_delayed_work(&wq_data.delayed_getMemInfo);
 	flush_workqueue( meminfo_queue );
 	destroy_workqueue( meminfo_queue );
-//	unregister_jprobe(&meminfo_jprobe);
-	printk("lttng-meminfo removed\n");
+	/* unregister the jprobe event*/
+	unregister_jprobe(&memalloc_jprobe);
+	unregister_jprobe(&memfree_jprobe);
+	printk("Lttng_meminfo unloaded\n");
 }
 module_exit(lttng_addons_meminfo_exit);
 
